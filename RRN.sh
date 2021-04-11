84ae2fecbf0522d7dae5b2fbbc8b05da243c92d9
ansty <kirillovaap@yandex.ru>
2021-04-11 23:09:44 +0300

    time

--- RRN.sh
+++ RRN.sh
@@ -1,8 +1,8 @@
 cd RRN
 pip install --upgrade pip
-pip install Pillow
+pip install Pillow opencv-python
 apt-get update && apt-get install -y python3-opencv
-apt install libgl1-mesa-glx
+apt install -y libgl1-mesa-glx
 apt-get install -y libglib2.0-0 libsm6 libxrender1  libxext6
 pip install scipy torchvision
 
