81160dbfdc5a4cb7e0f71f38032e4d52c3ab98cb
ansty <kirillovaap@yandex.ru>
2021-04-11 23:18:40 +0300

    script

--- RRN.sh
+++ RRN.sh
@@ -6,9 +6,6 @@ apt install -y libgl1-mesa-glx
 apt-get install -y libglib2.0-0 libsm6 libxrender1  libxext6
 pip install scipy torchvision
 
-python3 eval.py --test_dir /dataset --scene_name test1_bicubic_noise2 --image_out /output/RRN-5L --num_layers 5
-python3 eval.py --test_dir /dataset --scene_name test1_bicubic_noise2 --image_out /output/RRN-10L --num_layers 10
-
 python3 eval.py --test_dir /dataset --scene_name test1_gauss_noise2 --image_out /output/RRN-5L --num_layers 5
 python3 eval.py --test_dir /dataset --scene_name test2_bicubic_noise2 --image_out /output/RRN-5L --num_layers 5
 python3 eval.py --test_dir /dataset --scene_name test2_gauss_noise2 --image_out /output/RRN-5L --num_layers 5
