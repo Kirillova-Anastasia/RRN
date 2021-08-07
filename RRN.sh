1cf3e7a7553e3863a10aaa54ec7d29d99b7d9a12
ansty <kirillovaap@yandex.ru>
2021-08-07 23:14:12 +0300

    script

--- RRN.sh
+++ RRN.sh
@@ -5,3 +5,6 @@ apt-get update && apt-get install -y python3-opencv
 apt install -y libgl1-mesa-glx
 apt-get install -y libglib2.0-0 libsm6 libxrender1  libxext6
 pip install scipy torchvision
+
+python3 eval.py --test_dir /dataset --scene_name test1_gauss_noise2 --image_out /output/RRN-5L --num_layers 5
+python3 eval.py --test_dir /dataset --scene_name test1_gauss_noise2 --image_out /output/RRN-10L --num_layers 10
